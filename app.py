a41ffe6480e862ae41eec1f022d6589f6e4167be
James <james.waters@talktalk.net>
2017-07-28 13:14:30 +0100

    Moved the Server class to a new file, and added a basic Client class

--- app.py
+++ app.py
@@ -1,26 +1,17 @@
 from flask import Flask
 from flask_sockets import Sockets
 import gevent
+from classes import *
 
 app = Flask(__name__)
 sockets = Sockets(app)
-class Server():
-    def __init__(self):
-        self.clients = list()
-
-    def connect(self, client):
-        self.clients.append(client)
-
-    def update(self, client, data):
-        for c in self.clients:
-            if c != client:
-                c.send(data)
 
 svr = Server()
 
 @sockets.route('/connect')
 def connect(ws):
     svr.connect(ws)
+    print(type(ws))
     while not ws.closed:
         message = ws.receive()
         if message:
