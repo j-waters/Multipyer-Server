eae0e200a0cf6ebe2dce6ca95bc063840e540a0d
James <james.waters@talktalk.net>
2017-08-02 20:33:33 +0100

    Switched from spaces to tabs

--- __main__.py
+++ __main__.py
@@ -10,14 +10,14 @@ from gevent import monkey
 monkey.patch_all()
 
 def run_server():
-    if app.debug:
-        application = DebuggedApplication(app)
-    else:
-        application = app
+	if app.debug:
+		application = DebuggedApplication(app)
+	else:
+		application = app
 
-    server = pywsgi.WSGIServer(('127.0.0.1', 8000), application,
-                               handler_class=WebSocketHandler)
-    server.serve_forever()
+	server = pywsgi.WSGIServer(('127.0.0.1', 8000), application,
+							   handler_class=WebSocketHandler)
+	server.serve_forever()
 
 if __name__ == "__main__":
-    run_with_reloader(run_server)
+	run_with_reloader(run_server)
