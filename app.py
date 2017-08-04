494bc13534a00de558774e94ef786ba12e3da3fd
James <james.waters@talktalk.net>
2017-08-04 14:03:28 +0100

    Temporarily removed references to sql databases, as it is crashing the server

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ app.debug = True
 sockets = Sockets(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('OPENSHIFT_POSTGRESQL_DB_URL', '127.11.222.130:5432')
-db = SQLAlchemy(app)
-db.create_all()
+#db = SQLAlchemy(app)
+#db.create_all()
 
 svr = Server()
 
